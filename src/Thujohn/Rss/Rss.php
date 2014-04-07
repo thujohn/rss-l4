8c03400062167c3f6b7340a1c964cdb3626a4e4b
Jonathan Thuau <jonathan.thuau@gmail.com>
2014-04-07 22:15:45 +0200

    Update Rss.php

--- src/Thujohn/Rss/Rss.php
+++ src/Thujohn/Rss/Rss.php
@@ -2,16 +2,18 @@
 
 use Thujohn\Rss\Channel;
 
-class Rss{
+class Rss {
 	protected $version = '';
 	protected $encoding = '';
 	protected $channel = array();
 	protected $items = array();
 	protected $limit = 0;
 
-	public function feed($version, $encoding){
-		$this->version = $version;
+	public function feed($version, $encoding)
+	{
+		$this->version  = $version;
 		$this->encoding = $encoding;
+
 		return $this;
 	}
 
@@ -37,12 +39,15 @@ class Rss{
 	 * - skipHours
 	 * - skipDays
 	 */
-	public function channel($parameters){
-		if (!array_key_exists('title', $parameters) || !array_key_exists('description', $parameters) || !array_key_exists('link', $parameters)){
+	public function channel($parameters)
+	{
+		if (!array_key_exists('title', $parameters) || !array_key_exists('description', $parameters) || !array_key_exists('link', $parameters))
+		{
 			throw new \Exception('Parameter required missing : title, description or link');
 		}
 
 		$this->channel = $parameters;
+
 		return $this;
 	}
 
@@ -59,39 +64,59 @@ class Rss{
 	 * - pubDate
 	 * - source
 	 */
-	public function item($parameters){
-		if (empty($parameters)){
+	public function item($parameters)
+	{
+		if (empty($parameters))
+		{
 			throw new \Exception('Parameter missing');
 		}
 
 		$this->items[] = $parameters;
+
 		return $this;
 	}
 	
-	public function limit($limit) {
-		if(is_int($limit) and $limit > 0) {
+	public function limit($limit)
+	{
+		if (is_int($limit) and $limit > 0)
+		{
 			$this->limit = $limit;
 		}
+
 		return $this;
 	}
 
-	public function render(){
+	public function render()
+	{
 		$xml = new SimpleXMLElement('<?xml version="1.0" encoding="'.$this->encoding.'"?><rss version="'.$this->version.'" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:slash="http://purl.org/rss/1.0/modules/slash/"></rss>', LIBXML_NOERROR|LIBXML_ERR_NONE|LIBXML_ERR_FATAL);
 
 		$xml->addChild('channel');
-		foreach ($this->channel as $kC => $vC){
+
+		foreach ($this->channel as $kC => $vC)
+		{
 			$xml->channel->addChild($kC, $vC);
 		}
 
 		$items = $this->limit > 0 ? array_slice($this->items, 0, $this->limit) : $this->items;
-		foreach ($items as $item){
+
+		foreach ($items as $item)
+		{
 			$elem_item = $xml->channel->addChild('item');
-			foreach ($item as $kI => $vI){
+
+			foreach ($item as $kI => $vI)
+			{
 				$options = explode('|', $kI);
 
-				if (in_array('cdata', $options)){
+				if (in_array('cdata', $options))
+				{
 					$elem_item->addCdataChild($options[0], $vI);
-				}else{
+				}
+				elseif (strpos($options[0], ':') !== false)
+				{
+					$elem_item->addChild($options[0], $vI, 'http://purl.org/dc/elements/1.1/');
+				}
+				else
+				{
 					$elem_item->addChild($options[0], $vI);
 				}
 			}
@@ -100,11 +125,13 @@ class Rss{
 		return $xml;
 	}
 
-	public function save($filename){
+	public function save($filename)
+	{
 		return $this->render()->asXML($filename);
 	}
 
-	public function __toString(){
+	public function __toString()
+	{
 		return $this->render()->asXML();
 	}
 }
